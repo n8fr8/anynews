a271da77b38474acda8f6cbd49d6200a6e1815e0
n8fr8 <nathan@guardianproject.info>
2019-01-10 00:21:14 -0500

    add initial service worker cacheing support

--- js/anynews.js
+++ js/anynews.js
@@ -1,4 +1,8 @@
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
+
 function loadFeed (rssurl,templateName) {
 
 console.debug("loading: " + rssurl)
