8429b14af58b0caa90969170d0646d3e0952ed66
n8fr8 <nathan@guardianproject.info>
2019-01-10 02:24:30 -0500

    add support for headline browse view

--- js/anynews.js
+++ js/anynews.js
@@ -47,11 +47,14 @@ $.get(rssurl, function(data) {
 
      var tmpl = $.templates(templateId); // Get compiled template
 
+    var idx = 0;
+
     $xml.find("item").each(function() {
        var $this = $(this),
             item = {
                 title: $this.find("title").text(),
                 link: $this.find("link").text(),
+                guid: idx++,
                 description: $this.find("description").text(),
                 pubDate: $this.find("pubDate").text(),
                 author: $this.find("author").text(),
