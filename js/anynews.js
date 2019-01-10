ac42b0d44a0431a38db1f314849bf68fb3e6abbd
n8fr8 <nathan@guardianproject.info>
2019-01-10 00:55:52 -0500

    add manifest and icons for PWA support

--- js/anynews.js
+++ js/anynews.js
@@ -1,6 +1,33 @@
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+//  navigator.serviceWorker.register('service-worker.js');
+}
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+function showAddAppPrompt () {
+
+// Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+
+
 }
 
 function loadFeed (rssurl,templateName) {
@@ -8,6 +35,8 @@ function loadFeed (rssurl,templateName) {
 console.debug("loading: " + rssurl)
 
 $.get(rssurl, function(data) {
+
+    $('#output').empty();
     var $xml = $(data);
 
     var siteTitle;
@@ -16,7 +45,7 @@ $.get(rssurl, function(data) {
 	siteTitle = $(this).text();
     });
 
-    var tmpl = $.templates("#"+templateName); // Get compiled template
+    var tmpl = $.templates("#post-template"); // Get compiled template
 
     $xml.find("item").each(function() {
        var $this = $(this),
